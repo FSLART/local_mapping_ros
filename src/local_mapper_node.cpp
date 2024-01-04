06fd08e388b67710c9d16b84c77bc43456f702c0
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-01-04 15:26:57 +0000

    implement multithreaded executor

--- src/local_mapper_node.cpp
+++ src/local_mapper_node.cpp
@@ -1,9 +1,18 @@
 #include <rclcpp/rclcpp.hpp>
 #include <local_mapping_ros/LocalMapper.h>
 
+using namespace t24e::local_mapper;
+
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<t24e::local_mapper::LocalMapper>());
+
+    rclcpp::executors::MultiThreadedExecutor executor;
+
+    rclcpp::Node::SharedPtr node = std::make_shared<LocalMapper>();
+    executor.add_node(node);
+
+    executor.spin();
+
     rclcpp::shutdown();
     return 0;
 }
