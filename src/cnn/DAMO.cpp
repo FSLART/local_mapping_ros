9ab9be9612216dfafa570c40121b1f3ede63a026
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-01-08 19:25:54 +0000

    validate CUDA device availability in runtime. find out tensor shapes
    Extraction from inference results is not right. Found out the shapes now. Some processing missing

--- src/cnn/DAMO.cpp
+++ src/cnn/DAMO.cpp
@@ -11,24 +11,75 @@ namespace t24e::local_mapper::cnn {
 
         this->modelPath = modelPath;
         this->modelPathSet = true;
+
+        #ifdef WITH_CUDA
+            this->device = torch::Device(torch::kCUDA);
+            this->validateDevice();
+        #else
+            this->device = torch::Device(torch::kCPU);
+        #endif
     }
 
-    std::vector<bounding_box_t> DAMO::detectCones(const cv::Mat& img) {
+    void DAMO::validateDevice() {
+
+        if(this->device == torch::Device(torch::kCUDA)) {
+            if(!torch::cuda::is_available()) {
+                std::cerr << "CUDA is not available!" << std::endl;
+                throw std::runtime_error("CUDA is not available!");
+            }
+        }
+    }
+
+    std::vector<bounding_box_t> DAMO::detectCones(cv::Mat img) {
+
+        // resize the image to inference dimensions
+        cv::Mat resizedImg;
+        cv::resize(img, resizedImg, cv::Size(DETECTOR_WIDTH, DETECTOR_HEIGHT));
+
+        std::cout << this->device.type() << std::endl;
 
         // convert the opencv image to a tensor
-        at::Tensor tensorImage = torch::from_blob(img.data, {1, 3, img.rows, img.cols}, at::kFloat);
-        // move the tensor to the GPU
-        tensorImage = tensorImage.to(at::kCUDA);
+        at::Tensor tensorImage = torch::from_blob(resizedImg.data, {1, 3, resizedImg.rows, resizedImg.cols}, at::kFloat);
+        std::cout << "Tensor shape: " << tensorImage.sizes() << std::endl;
+
+        // check the device is available
+        this->validateDevice();
+
+        try {
+            // move the tensor to the intended device
+            tensorImage = tensorImage.to(this->device);
+        } catch(const std::exception& e) {
+            std::cerr << "Error moving the tensor to the device: " << e.what() << std::endl;
+            throw std::runtime_error("Error moving the tensor to the device!");
+        }
 
         // convert to a vector
         std::vector<torch::jit::IValue> input;
         input.emplace_back(tensorImage);
 
         // execute the model
-        torch::Tensor outputs = this->torchModule.forward(input).toTensor();
+        auto inference = this->torchModule.forward(input);
+
+        auto outputs = inference.toTuple();
+
+        /*
+            tensors:
+            [0]: [1, 8400, 5] -> class probabilities
+            [1]: [1, 8400, 4] -> box origin and dimensions
+        */
+
+        for(size_t i = 0; i < outputs->elements().size(); i++) {
+            torch::Tensor elem = outputs->elements()[i].toTensor();
+
+            std::cout << "Elem " << i << ": " << elem.sizes() << std::endl;
+        }
+
+        std::vector<bounding_box_t> bounding_boxes;
+
+        /*
+        at::Tensor outputs = inference.toTensor();
 
         // extract the bounding boxes
-        std::vector<bounding_box_t> bounding_boxes;
         for(int i = 0; i < outputs.size(0); i++) {
             bounding_box_t box;
             box.box.first.first = outputs[i][0].item().toInt();
@@ -39,7 +90,7 @@ namespace t24e::local_mapper::cnn {
 
             box.label = outputs[i][4].item().toInt();
             bounding_boxes.push_back(box);
-        }
+        }*/
 
         // return a vector of bounding boxes
         return bounding_boxes;
@@ -54,7 +105,12 @@ namespace t24e::local_mapper::cnn {
         // load the torchscript model
         try {
             std::cout << "Loading the TorchScript module at " << this->modelPath << std::endl;
-            this->torchModule = torch::jit::load(this->modelPath);
+            this->validateDevice();
+
+            this->torchModule = torch::jit::load(this->modelPath, this->device);
+            
+            this->torchModule.eval(); // set the model to evaluation mode
+
         } catch(const c10::Error& e) {
             std::cerr << "Error loading the TorchScript module: " << e.what() << std::endl;
             throw std::runtime_error("Error loading the TorchScript module!");
