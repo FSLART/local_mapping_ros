3650780ac27c6c09e600cf28c78ae32ab38b6989
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-01-12 21:11:10 +0000

    lart-451 implement row entropy

---
+++ src/post_processing/Filtering.cpp
@@ -0,0 +1,19 @@
+//
+// Created by carlostojal on 12-01-2024.
+//
+
+#include <local_mapping_ros/post_processing/Filtering.h>
+
+namespace t24e::local_mapper::post_processing {
+
+    Filtering::entropyRow(torch::Tensor& row) {
+        // calculate the entropy of the row
+        float entropy = 0;
+        for(int i = 0; i < row.size(0); i++) {
+            float p = row[i].item<float>();
+            entropy += p * std::log(p);
+        }
+        return -entropy;
+    }
+
+};
